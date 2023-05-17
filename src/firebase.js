595e2064dacf32bd2323ff4079afde53762f8543
Aarize Siddique <97665948+Aarize29@users.noreply.github.com>
2023-05-17 20:47:10 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCsZzRXLR7HeuqO4nS5wivBjiFWvXkTeCQ",
-  authDomain: "newproject-c370a.firebaseapp.com",
-  projectId: "newproject-c370a",
-  storageBucket: "newproject-c370a.appspot.com",
-  messagingSenderId: "490755644962",
-  appId: "1:490755644962:web:9fc4ad1220fcbc7b4af47b",
-  measurementId: "G-2Y93W2R8GF"
+  " your credentials "
 };
 
 const app = initializeApp(firebaseConfig);
